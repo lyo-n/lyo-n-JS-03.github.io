59fefbcc1988f81ea353994988fb19e60e5fcaec
lyo-n <1y0mmmm@gmail.com>
2020-07-20 18:41:13 +0300

    module-03/2

---
+++ JS/task-4.js
@@ -0,0 +1,27 @@
+const countTotalSalary = function(employees) {
+   let sum=0;
+   for (let employee of Object.values(employees)) {
+    sum += employee;
+  }
+   return sum;
+  };
+    /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(countTotalSalary({})); // 0
+  
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
